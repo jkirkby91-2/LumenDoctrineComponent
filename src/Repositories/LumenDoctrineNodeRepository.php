b472238a229add201fb76df610980985d91f21df
James Kirkby <me@jameskirkby.com>
2016-10-01 18:16:20 +0100

    make repository implement contract correctly

--- src/Repositories/LumenDoctrineNodeRepository.php
+++ src/Repositories/LumenDoctrineNodeRepository.php
@@ -4,6 +4,7 @@ namespace Jkirkby91\LumenDoctrineComponent\Repositories;
 
 use Jkirkby91\DoctrineNodeEntity\DoctrineNodeRepository;
 use Jkirkby91\LumenDoctrineComponent\Entities\LumenDoctrineNode;
+use Jkirkby91\Boilers\NodeEntityBoiler\EntityContract AS Entity;
 
 /**
  * Class LumenDoctrineRepository
@@ -15,7 +16,7 @@ class LumenDoctrineNodeRepository extends DoctrineNodeRepository
      * @param array|\Jkirkby91\Boilers\NodeEntityBoiler\EntityContract|\Psr\Http\Message\ServerRequestInterface $entity
      * @return $LumenDoctrineNode
      */
-    public function create($entity)
+    public function create(Entity $entity)
     {
         $LumenDoctrineNode = new LumenDoctrineNode();
         $LumenDoctrineNode->setNodeType($entity->nodeType);
@@ -28,7 +29,7 @@ class LumenDoctrineNodeRepository extends DoctrineNodeRepository
      * @param int|\Jkirkby91\Boilers\NodeEntityBoiler\EntityContract|\Psr\Http\Message\ServerRequestInterface $entity
      * @return int|\Jkirkby91\Boilers\NodeEntityBoiler\EntityContract|\Psr\Http\Message\ServerRequestInterface
      */
-    public function update($entity)
+    public function update(Entity $entity)
     {
         $LumenDoctrineNode = $this->find($entity);
         $LumenDoctrineNode->setNodeType($LumenDoctrineNode->nodeType);
